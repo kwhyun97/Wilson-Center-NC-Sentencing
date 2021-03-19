b4fea60d4fa6d2a710fbed77cef48e241f21adc1
Kiwan Hyun <kh303@duke.edu>
2021-03-20 01:22:44 +0900

    initial exploration

--- data_cleaning.R
+++ data_cleaning.R
@@ -1,1 +1,13 @@
 sentencing <- readRDS("data/hxmisd9919_request.RDS")
+
+# felony
+felony <- sentencing %>% 
+  filter(chrgtype == 1)
+
+# misdemeanor
+misdemeanor <- sentencing %>% 
+  filter(chrgtype == 2)
+
+# durham data
+durham <- sentencing %>% 
+  filter(COUNTY == "310")
